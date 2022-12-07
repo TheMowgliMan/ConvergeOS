19b4ac94fdfa6dc02c5acd44325bc381d1f02796
Kkidslogin <96443213+TheMowgliMan@users.noreply.github.com>
2022-12-07 06:04:35 -0800

    Add HELP cmdproc command and improve APPENDing

--- cmdproc.py
+++ cmdproc.py
@@ -14,7 +14,6 @@ class CmdprocInterpreter:
         out = ""
         for line in string.split("\n"):
             data = str(line).split("|")
-            print(data)
 
             command_iter = 0
             for command in data:
@@ -27,12 +26,23 @@ class CmdprocInterpreter:
                     del cmdinp[0]
 
                     if command_iter > 0:
-                        if data[command_iter - 1].split("-")[0] == "APPEND":
+                        if data[command_iter - 1].split("-")[0] == "APPEND" and len(data[command_iter - 1].split("-")) == 0:
                             out = out + self.FILE(cmdinp)
                         else:
                             out = self.FILE(cmdinp)
                     else:
                         out = self.FILE(cmdinp)
+                elif cmdinp[0] == "HELP":
+                    del cmdinp[0]
+
+                    if command_iter > 0:
+                        if data[command_iter - 1].split("-")[0] == "APPEND" and len(data[command_iter - 1].split("-")) == 0:
+                            out = out + self.HELP(cmdinp)
+                        else:
+                            out = self.HELP(cmdinp)
+                    else:
+                        out = self.HELP(cmdinp)
+
 
                 command_iter += 1
 
@@ -50,6 +60,17 @@ class CmdprocInterpreter:
                 print("File '" + inputs[0] + "' does not exist.")
         else:
             print("Incorrect number of arguments for FILE.")
+
+    def HELP(self, inputs):
+        if len(inputs) == 0:
+            return "All Commands:\nAPPEND\nFILE\nHELP"
+        else:
+            if input[0] == "APPEND":
+                return "APPEND-ar1-arg2: Appends each argument (separated by a space) to the output of the cmdproc set. Has special behavior for other commands."
+            elif input[0] == "FILE":
+                return "FILE-filename: Sets the output to the data in the chosen file. If preceded by an APPEND command with no arguments, appends it instead."
+            elif input[0] == "HELP":
+                return "HELP-command(optional). Sets the output to either info about the chosen command or a list of commands. If preceded by an APPEND command with no arguments, appends it instead."
 
 # Testing code
 if __name__ == "__main__":
